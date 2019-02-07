376e7d885307216ef03e1800fa05361ff1a58fe7
Zi Wei Zhang <ziwei.lotus@gmail.com>
2019-02-07 12:43:02 -0500

    Update buy.js

--- pages/actions/buy.js
+++ pages/actions/buy.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
+import { Button, Message } from 'semantic-ui-react'
 import Layout from '../../components/Layout'
 
 class Buy extends Component {
     render() {
         return (
             <Layout>
-                <div style={{ textAlign: 'center', color: 'green', marginTop: '70px' }}>
-                    <h1>Limited Time Offer!</h1>
-                    <h2>Coming soon...</h2>
-                </div>
+                <Message color="green" size="massive" style={{ textAlign: 'center', marginTop: '68px' }}>
+                    <Message.Header><h1><b>ICO Crowdsale - Kitten Token</b></h1></Message.Header>
+                    <h1><b>Limited Time Offer!</b></h1>
+                    <a href="https://ziweidream.github.io/token-sale-react/">
+                        <Button color='green' size="massive"><b>Enter</b></Button>
+                    </a>
+                </Message>
             </Layout>
         )
     }
